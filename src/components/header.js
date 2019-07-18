7883ce53555f7f306c53749321e7734a74260d60
Brett E Stockmeier <stockmeierb@gmail.com>
2019-07-17 23:40:00 -0500

    header shadows

--- src/components/header.js
+++ src/components/header.js
@@ -17,8 +17,11 @@ const styles = {
     backgroundColor: "#F8F8F8",
     justifyContent: "center",
     alignItems: "center",
-    height: 60,
-    paddingTop: 15
+    height: 70,
+    paddingTop: 25,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2
   },
   textStyle: {
     fontSize: 20
