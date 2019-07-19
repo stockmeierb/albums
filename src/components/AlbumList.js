a3811a51160792cb749d10fc194b6169325a8e65
Brett E Stockmeier <stockmeierb@gmail.com>
2019-07-19 15:05:51 -0500

    verifyed axios request

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
+import axios from "axios";
 
 class AlbumList extends Component {
-  componentDidMount() {}
+  componentDidMount() {
+    axios
+      .get("http://rallycoding.herokuapp.com/api/music_albums")
+      .then(response => console.log(response));
+  }
 
   render() {
     return (
