a19f93a4e9b1e17c36e6fa45365a12dd77f6cfd7
Brett E Stockmeier <stockmeierb@gmail.com>
2019-07-19 16:05:01 -0500

    created component state

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -3,13 +3,16 @@ import { View, Text } from "react-native";
 import axios from "axios";
 
 class AlbumList extends Component {
+  state = { albums: [] };
+
   componentDidMount() {
     axios
       .get("http://rallycoding.herokuapp.com/api/music_albums")
-      .then(response => console.log(response));
+      .then(response => this.setState({ albums: response.data }));
   }
 
   render() {
+    console.log(this.state);
     return (
       <View>
         <Text>AlbumList</Text>
