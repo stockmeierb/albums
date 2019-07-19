93803b9cdd78c8714cff90f6631362abccd28d92
Brett E Stockmeier <stockmeierb@gmail.com>
2019-07-18 23:08:46 -0500

    added lifecycle method to AlbumList component

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { View, Text } from "react-native";
 
 class AlbumList extends Component {
+  componentDidMount() {}
+
   render() {
     return (
       <View>
