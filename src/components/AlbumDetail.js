910cc735a6b7ca73072bd2248c9235740b5efa8d
Brett E Stockmeier <stockmeierb@gmail.com>
2019-07-20 22:24:38 -0500

    add album artwork to album detail

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -4,18 +4,28 @@ import Card from "./Card";
 import CardItem from "./CardItem";
 
 const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image } = album;
-  const { headerStyles, thumbStyle } = styles;
+  const { title, artist, thumbnail_image, image } = album;
+  const {
+    headerStyles,
+    headerTextStyle,
+    thumbStyle,
+    thumbBoxStyle,
+    imageStyle
+  } = styles;
+
   return (
     <Card>
       <CardItem>
-        <View>
+        <View style={thumbBoxStyle}>
           <Image source={{ uri: thumbnail_image }} style={thumbStyle} />
         </View>
         <View style={headerStyles}>
-          <Text>{title}</Text>
+          <Text style={headerTextStyle}>{title}</Text>
           <Text>{artist}</Text>
         </View>
+      </CardItem>
+      <CardItem>
+        <Image source={{ uri: image }} style={imageStyle} />
       </CardItem>
     </Card>
   );
@@ -26,7 +36,15 @@ const styles = {
     flexDirection: "column",
     justifyContent: "space-around"
   },
-  thumbStyle: { height: 50, width: 50 }
+  headerTextStyle: { fontSize: 18 },
+  thumbStyle: { height: 50, width: 50 },
+  thumbBoxStyle: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: { height: 300, flex: 1, width: null }
 };
 
 export default AlbumDetail;
